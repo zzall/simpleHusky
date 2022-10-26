19553459b07390872fbf7b04a8f1172db1f7a15a
zzall <1809642355@qq.com>
2022-10-26 17:51:22 +0800

    feat:新增更完整示例

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # simpleHusky
 
+30 行代码实现简易 husky
 
-30行代码实现简易husky
+## 定义 githooks
 
-## 定义githooks
+在 package.json 文件中定义
 
-在package.json文件中定义
 ```javascript
  "zzzgithooks":{
     "pre-commit":"npm run test3"
   }
 ```
 
-## 注册githooks
+## 注册 githooks
 
 ```bash
 npm run zzzgithooks
@@ -20,11 +20,15 @@ npm run zzzgithooks
 
 ## git commit 验证
 
-
 添加文件改动
 
 ```bash
 git commit -m 'xxx'
 ```
 
+## 更完整功能在`./example2`中
 
+跟上面的实现相比有以下不同
+
+- 新增了 cli 命令
+- 采用了 shell 脚本而不是 node 脚本
---
+++ example2/index.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const fs = require("fs");
+const child_process = require("child_process");
+
+const exec = child_process.exec;
+
+const hooksPath = path.resolve(__dirname, ".zzz");
+
+const createHookFile = (filepath) => {
+  filepath = filepath || path.resolve(hooksPath, "pre-commit");
+  const template = `
+  #!/usr/bin/env sh
+  npm test
+  `;
+  fs.writeFile(filepath, template, (err) => {
+    if (err) throw err;
+    console.log("文件创建成功");
+    exec(`chmod u+x ${filepath}`);
+  });
+};
+
+if (!fs.existsSync(hooksPath)) {
+  fs.mkdir(hooksPath, { recursive: true }, (err) => {
+    if (err) throw err;
+    createHookFile();
+    exec(`git config core.hooksPath ${hooksPath}`);
+  });
+} else {
+  createHookFile();
+  exec(`git config core.hooksPath ${hooksPath}`);
+}
